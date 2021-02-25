e251674e5c3fbf1670451c10bee223da6a293283
阿忆 <296488320@qq.com>
2021-02-25 18:25:18 +0800

    first commit

---
+++ TestJavac/src/main/java/com/example/testjavac/MyClass.java
@@ -0,0 +1,16 @@
+package com.example.testjavac;
+
+
+import java.util.Locale;
+
+/**
+ * 作者:zhenxi
+ */
+@SuppressWarnings("all")
+public class MyClass {
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("12345");
+    }
+}
---
+++ javacSrc/src/main/java/com/sun/tools/javac/util/CLogUtils.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javac.util;
+
+/**
+ * Created by Lyh on
+ * 2021/2/17
+ */
+public class CLogUtils {
+    public static void log(String message){
+        System.out.println(message);
+    }
+}
